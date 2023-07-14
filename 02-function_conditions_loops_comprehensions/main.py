1e3c1cf48a3aa1990f7308a352867cfcade29e56
Mehmet Emin ÇİL <47079995+emincilm@users.noreply.github.com>
2023-07-14 14:54:00 +0300

    Add files via upload

--- 02-function_conditions_loops_comprehensions/main.py
+++ 02-function_conditions_loops_comprehensions/main.py
@@ -501,3 +501,80 @@ for n in numbers:
 
 # Bir Veri Setindeki değişken isimlerini değiştirmek
 
+# before:
+# [ 'total', 'speeding', 'alcohol', 'not_dictracted', 'no_previous', 'ins_premium', 'ins_loses', 'abbrev']
+# [ 'TOTAL', 'SPEADING', 'ALCOHOL', 'NOT_DICTRACTED', 'NO_PREVIOUS', 'INS_PREMIUM', 'INS_LOSES', 'ABBREV']
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns
+
+for col in df.columns:
+    print(col.upper())
+
+A = []
+
+for col in df.columns:
+    A.append(col.upper())
+
+df.columns =  A
+
+df = sns.load_dataset("car_crashes")
+df.columns = [col.upper() for col in df.columns]
+
+# İsminde "INS" olan değişkenlerin başına FLAG diğerlerine NO_FLAG eklemek istiyoruz
+# before:  # [ 'TOTAL', 'SPEADING', 'ALCOHOL', 'NOT_DICTRACTED', 'NO_PREVIOUS', 'INS_PREMIUM', 'INS_LOSES', 'ABBREV']
+# after:   # [ 'NO_FLAG_TOTAL', 'NO_FLAG_SPEADING', 'NO_FLAG_ALCOHOL', 'NO_FLAG_NOT_DICTRACTED', 'NO_FLAG_NO_PREVIOUS', 'FLAG_INS_PREMIUM', 'FLAG_INS_LOSES', 'NO_FLAG_ABBREV']
+
+[col for col in df.columns if "INS" in col]
+
+["FLAG_"+ col for col in df.columns if "INS" in col]
+
+["FLAG_"+ col  if "INS" in col else "NO_FLAG_"+ col for col in df.columns]
+
+df.columns = ["FLAG_"+ col  if "INS" in col else "NO_FLAG_"+ col for col in df.columns]
+
+# Amaç key'i string , valuesi aşağıdaki gibi bir liste olan sözlük oluşturmak
+# Bu işlemi sadece sayısal değişkenler için yapmak istiyoruz
+# Output:
+# { 'total':['mean','min', 'max', 'var'] ,
+# 'speeding':['mean','min', 'max', 'var'] ,
+# 'alcohol':['mean','min', 'max', 'var'] ,
+# 'not_dictracted':['mean','min', 'max', 'var'] ,
+# 'no_previous':['mean','min', 'max', 'var'] ,
+# 'ins_premium':['mean','min', 'max', 'var'] ,
+# 'ins_loses':['mean','min', 'max', 'var'] }
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns
+
+num_cols =  [col for col in df.columns if df[col].dtype != "O"]
+soz = {}
+agg_list =  ["mean", "min", "max", "sum"]
+
+for col in num_cols:
+    soz[col] =  agg_list
+
+#kısa yol
+
+new_dict = {col: agg_list for col in num_cols}
+
+df[num_cols].head()
+
+df[num_cols].agg(new_dict)
+
+wages = [1000,2000,3000,4000,5000]
+new_wages =  lambda  x: x*0.20+x
+list(map(new_wages,wages))
+
+students = ["Denise", "Arsem","Tony","Audrey"]
+low =  lambda x: x[0].lower()
+print(list(map(low,students)))
+
+dictn = {"Denise": 10, "Arsen": 12, "Tony": 15, "Audrey": 17}
+new_dict =  {k: v*2 + 3 for (k, v) in dictn.items()}
+new_dict
+
+numbers = range(1,10)
+numsda = {n: n ** 2 for n in numbers if n %2 != 0}
